46b417de719bf115b6f66ee378b7c36f94937f38
kibzrael <kibraphael7@gmail.com>
2021-08-31 23:13:17 +0300

    first refactor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:video_trimmer/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(VideoTrimmerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
